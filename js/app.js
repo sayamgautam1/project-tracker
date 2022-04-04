e28b36254ed73ec6403d7dbaeade2ccb36fe8cf4
Sayam Gautam <sayamgautam1@gmail.com>
2022-04-04 21:01:02 +0930

    display time every 1 sec

--- js/app.js
+++ js/app.js
@@ -3,3 +3,14 @@
 $(function () {
   $("#datepicker").datepicker();
 });
+
+// header section
+
+let datetimeEl = $("#date-time");
+// datetimeEl.text(today.format("MMM Do, YYYY"));
+
+// update seconds every 60 seconds
+
+setInterval(() => {
+  datetimeEl.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+}, 1000);
